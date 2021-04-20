1f96d58596649b60baab38f81a30e553aadb9a9c
sorkierkegaard <es.luismiguelbaez@gmail.com>
2021-04-20 09:48:55 -0500

    Edge detection filter (kernel) was added

--- filter.c
+++ filter.c
@@ -9,6 +9,10 @@ char* IMG_OUTPUT;
 float ARG;
 int THREADS;
 
+float kernel[3][3] = {{-1,-1,-1}, 
+                      {-1, 8,-1},
+                      {-1,-1,-1}};
+
 int main(int argc, char *argv[]) {
 
     if(argc < 5) {
@@ -25,6 +29,7 @@ int main(int argc, char *argv[]) {
     // TODO:
 
     sod_img imgIn = sod_img_load_from_file(IMG_INPUT, SOD_IMG_COLOR);
+    sod_img imgOut = sod_img_load_from_file(IMG_INPUT, SOD_IMG_COLOR);
 
     if (imgIn.data == 0) {
         // Validate that the image exists
@@ -32,29 +37,32 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    // Modify the image
+    // Modificar la imagen
     const int TAM_COLORES = 9;
     float colores[9];
 
     printf("Image size %dx%d\n", imgIn.w, imgIn.h);
 
-    for(int i = 0; i < imgIn.w/2; ++i) {
-        for(int j = 0; j < imgIn.h; ++j) {
-            float R = sod_img_get_pixel(imgIn, i, j, 0);
-            float G = sod_img_get_pixel(imgIn, i, j, 1);
-            float B = sod_img_get_pixel(imgIn, i, j, 2);
-
-            float mean = (R+G+B) / 3.0;
-            sod_img_set_pixel(imgIn, i, j, 0, mean);
-            sod_img_set_pixel(imgIn, i, j, 1, mean);
-            sod_img_set_pixel(imgIn, i, j, 2, mean);
+    for(int y = 1; y < imgIn.h-1; ++y) {
+        for(int x = 1; x < imgIn.w-1; ++x) {
+            float sum = 0.0;
+            for(int ky = -1; ky <= 1; ++ky){
+                for(int kx = -1; kx <= 1; ++kx){
+                    float val = sod_img_get_pixel(imgIn, x+kx, y+ky, 0);
+                    sum += kernel[ky+1][kx+1] * val;
+                }
+            }
+            sod_img_set_pixel(imgOut, x, y, 0, abs(sum));            
+            sod_img_set_pixel(imgOut, x, y, 1, abs(sum));            
+            sod_img_set_pixel(imgOut, x, y, 2, abs(sum));            
         }
     }
-
+    // Save image ouput
     sod_img_save_as_png(imgIn, IMG_OUTPUT);
 
     // Free the Memory
     sod_free_image(imgIn);
+    sod_free_image(imgOut);
 
     return 0;
 }
